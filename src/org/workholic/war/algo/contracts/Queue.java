7414351410875e5bc4c19176fcbb6e9d9df537bc
Anish Singh <Anish.Singh@adesa.com>
2020-08-29 20:07:18 +0530

    Added Queue utility methods

--- src/org/workholic/war/algo/contracts/Queue.java
+++ src/org/workholic/war/algo/contracts/Queue.java
@@ -1,5 +1,48 @@
 package org.workholic.war.algo.contracts;
 
-public interface Queue {
+/**
+ * This interface provides methods which provides functionality related to Queue
+ * 
+ * @author Anish Singh(anish2u2@gmail.com)
+ *
+ */
+public interface Queue<T> {
+
+	/**
+	 * This method will offer element to Queue.
+	 * 
+	 * @param data
+	 * @return
+	 */
+	public boolean offer(T data);
+
+	/**
+	 * This method will return head element of the Queue and remove the element.
+	 * 
+	 * @return
+	 */
+	public T poll();
+
+	/**
+	 * This method will return head element of the Queue but not remove the element
+	 * from the Queue.
+	 * 
+	 * @return
+	 */
+	public T peek();
+
+	/**
+	 * This method will return size of the Queue.
+	 * 
+	 * @return
+	 */
+	public int size();
+
+	/**
+	 * This method will return that Queue is empty or not.
+	 * 
+	 * @return
+	 */
+	public boolean isEmpty();
 
 }
